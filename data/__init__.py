7f72ef54f56b3600bef01526a3ed676b9899ba28
ForJadeForest <1113480910@qq.com>
2022-07-15 20:20:40 +0800

    change the logic of import module

--- data/__init__.py
+++ data/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 __all__ = [
     f.stem
     for f in Path(__file__).parent.glob("*.py")
-    if "_" not in f.stem
+    if not f.stem.startswith('_')
 ]
 print(__all__)
 del Path
