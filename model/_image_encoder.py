8daac49ec578848a15e50a13477254de5c84903a
ForJadeForest <1113480910@qq.com>
2022-07-24 09:44:56 +0800

    add the initialize function. add the embedding projection and the hidden_projection for student model.

--- model/_image_encoder.py
+++ model/_image_encoder.py
@@ -2,25 +2,46 @@ from torch import nn
 
 try:
     from ._common import VisionTransformer
+    from ._utils import output_filter
 except ModuleNotFoundError:
     from _common import VisionTransformer
+    from _utils import output_filter
 
 
 class ImageEncoder(nn.Module):
-    def __init__(self, vision_width, image_resolution, vision_patch_size, vision_layers, embed_dim):
+    def __init__(self, is_student, vit_paras, tea_transformer_width=None):
         super().__init__()
-        vision_heads = vision_width // 64
-        self.visual = VisionTransformer(
-            input_resolution=image_resolution,
-            patch_size=vision_patch_size,
-            width=vision_width,
-            layers=vision_layers,
-            heads=vision_heads,
-            output_dim=embed_dim
-        )
+        self.visual = VisionTransformer(**vit_paras)
+        self.is_student = is_student
+        self.embedding_projection = None
+        self.hidden_projection = None
+        if is_student:
+            self.embedding_projection = nn.Linear(vit_paras['width'], tea_transformer_width)
+            self.hidden_projection = nn.Linear(vit_paras['width'], tea_transformer_width)
+        self.initialize_parameters()
 
-    def encode_image(self, image):
-        return self.visual(image)
+    def initialize_parameters(self):
+        nn.init.normal_(self.visual.class_embedding, std=0.02)
+        nn.init.normal_(self.visual.positional_embedding, std=0.01)
 
-    def forward(self, image):
-        return self.encode_image(image)
+        proj_std = (self.visual.transformer.width ** -0.5) * ((2 * self.visual.transformer.layers) ** -0.5)
+        attn_std = self.visual.transformer.width ** -0.5
+        fc_std = (2 * self.visual.transformer.width) ** -0.5
+        for block in self.visual.transformer.resblocks:
+            nn.init.normal_(block.attn.in_proj_weight, std=attn_std)
+            nn.init.normal_(block.attn.in_proj_bias, std=attn_std)
+            nn.init.normal_(block.attn.out_proj.weight, std=proj_std)
+            nn.init.normal_(block.mlp.c_fc.weight, std=fc_std)
+            nn.init.normal_(block.mlp.c_proj.weight, std=proj_std)
+
+    def encode_image(self, image, only_last_state=True):
+        last_state, attention_maps, representations, embedding = self.visual(image)
+
+        if only_last_state:
+            return last_state
+
+        return output_filter(self.is_student, representations, self.embedding_projection, embedding, attention_maps,
+                             last_state, self.hidden_projection)
+
+    def forward(self, image, only_last_state=True):
+        return self.encode_image(image, only_last_state)
