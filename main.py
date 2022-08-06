59172be691e6d97b2a219c78dc6e3ee3513f6036
ForJadeForest <1113480910@qq.com>
2022-08-06 10:54:22 +0800

    delete save config callback

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 # 导入torch中所有的优化器
 pl_cli.OPTIMIZER_REGISTRY.register_classes(module=torch.optim, base_cls=torch.optim.Optimizer)
 pl_cli.CALLBACK_REGISTRY.register_classes(module=pl.callbacks, base_cls=pl.Callback)
-cli = pl_cli.LightningCLI(seed_everything_default=2022)
+cli = pl_cli.LightningCLI(seed_everything_default=2022, save_config_overwrite=True)
