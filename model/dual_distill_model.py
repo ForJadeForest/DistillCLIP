cefdb540c85572beb68bc5a41a87c9b22ea3aeee
ForJadeForest <1113480910@qq.com>
2022-11-20 16:18:43 +0800

    1. add speed_test for CLIP model 2. modify the forward function. delete the only last state parameter.

--- model/dual_distill_model.py
+++ model/dual_distill_model.py
@@ -5,33 +5,34 @@ import torch
 import transformers
 import wandb
 from pytorch_lightning.loggers import WandbLogger, TensorBoardLogger
-from torch import optim
+from torch import optim, nn
 from torchmetrics import Accuracy
-from torchmetrics.functional import accuracy
 
 from ._loss import LossCalculator
+from ._metrics import cal_flop, cal_speed
 from ._utils import teacher_load
 from .component.clip_model import CLIPModel
 
 
 class DualDistillModel(pl.LightningModule):
-    def __init__(self, vit_paras: Dict, text_encoder_para: Dict, teacher_name: str, loss_control_para: Dict,
-                 download_root: str, lr: float = 1e-3, map_type: str = 'mid', init_type=None):
+    def __init__(self, image_student: nn.Module, text_student: nn.Module, need_layers: List, teacher_name: str,
+                 loss_control_para: Dict,
+                 download_root: str, lr: float = 1e-3, map_type: Optional[str] = None, init_type: Optional[str] = None,
+                 norm=False):
         super().__init__()
-        self.__dict__.update(locals())
         self.save_hyperparameters(ignore=['student_encoder'])
 
         # 定义模型
-        self.student = CLIPModel(True, vit_paras, text_encoder_para, tea_transformer_width=768)
+        self.student = CLIPModel(True, image_student, text_student, norm)
         self.teacher_name = teacher_name
-        self.teacher = teacher_load(teacher_name, download_root, 'all')
+        self.teacher = teacher_load(teacher_name, download_root, 'all', need_layers=need_layers)
         for p in self.teacher.parameters():
             p.requires_grad = False
 
         self.loss_control = LossCalculator(**loss_control_para)
         self.need_return_para = self.loss_control.get_control_output()
         # 定义指标
-        self.k_list = [i for i in [1, 5, 10]]
+        self.k_list = [i for i in [1, 2, 3, 4, 5, 10]]
         self.acc_metrics = torch.nn.ModuleList()
         for k in self.k_list:
             self.acc_metrics.append(Accuracy(top_k=k))
@@ -46,12 +47,28 @@ class DualDistillModel(pl.LightningModule):
                 wandb.save('./model/component/*.py')
             elif isinstance(self.logger, TensorBoardLogger):
                 self.logger.log_hyperparams(self.hparams, {"hp/stu_acc_top1": 0, "hp/stu_acc_top10": 0})
+        dummy_input = (
+            torch.rand(size=(1, 77), device=self.device), torch.rand(size=(1, 3, 224, 224), device=self.device))
+        self.speed_test(self.student, dummy_input, pre_fix='stu_')
+        self.speed_test(self.teacher, dummy_input, pre_fix='tea_')
+
+    def speed_test(self, model, dummy_input, pre_fix='stu_'):
+        flops, param = cal_flop(model, dummy_input)
+        mean_syn, std_syn, mean_fps = cal_speed(self.student, dummy_input)
+        metric_dict = {
+            pre_fix + 'flops': flops,
+            pre_fix + 'param': param,
+            pre_fix + 'mean_times': mean_syn,
+            pre_fix + 'std_times': std_syn,
+            pre_fix + 'mean_fps': mean_fps
+        }
+        self.log_dict(metric_dict, sync_dist=True)
 
     def forward(self, inputs):
         image, text = inputs
         text = text.squeeze(dim=1)
-        student_outs = self.student(text, image, self.need_return_para, only_last_state=False)
-        teacher_outs = self.teacher(text, image, self.need_return_para, only_last_state=False)
+        student_outs = self.student(text, image, self.need_return_para)
+        teacher_outs = self.teacher(text, image, self.need_return_para)
         return student_outs, teacher_outs
 
     def training_step(self, inputs, batch_idx):
