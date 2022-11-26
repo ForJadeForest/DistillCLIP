860fefe8ef8c8a2dba7a5fec04cc028fd036904e
ForJadeForest <1113480910@qq.com>
2022-11-26 12:34:30 +0800

    support the resnet encoder training

--- model/distil_model.py
+++ model/distil_model.py
@@ -8,30 +8,29 @@ from matplotlib import pyplot as plt
 from pytorch_lightning.loggers import WandbLogger, TensorBoardLogger
 from pytorch_lightning.utilities import rank_zero_only
 from torch import optim, nn
-from torchmetrics import Accuracy
 from torchmetrics.functional import accuracy
 
 from ._loss import LossCalculator
 from ._metrics import cal_speed, cal_flop
 from ._utils import teacher_load
+from .component.image_encoder import ImageEncoder
 
 
 class DistillModel(pl.LightningModule):
     def __init__(self, student_encoder: torch.nn.Module,
                  teacher_name: str, loss_control_para: Dict, download_root: str, norm: bool,
-                 teacher_need_layers: List, model_type: str = 'text', map_type: str = 'mid', init_type=None,
+                 teacher_need_layers: List = None, model_type: str = 'image', map_type: str = None, init_type=None,
                  warm_steps=10, total_steps=200, weight_decay=1e-3, lr: float = 1e-3):
         super().__init__()
         if model_type not in ['text', 'image']:
             raise ValueError(f"the model_type should in ['text', 'image'], bug got {model_type}")
         self.save_hyperparameters(ignore=['student_encoder'])
 
-        # 定义模型
         self.student = student_encoder
         self.teacher_name = teacher_name
         self.teacher = teacher_load(teacher_name, download_root, model_type,
                                     need_layers=teacher_need_layers)
-        if len(self.teacher.need_layers) != len(self.student.need_layers):
+        if isinstance(self.teacher, ImageEncoder) and len(self.teacher.need_layers) != len(self.student.need_layers):
             raise ValueError(
                 f'the teacher need_layers length is not equal to student need_layers length. '
                 f'But get teacher: {self.teacher.need_layers}, student: {self.student.need_layers}')
@@ -40,11 +39,7 @@ class DistillModel(pl.LightningModule):
         for p in self.teacher.parameters():
             p.requires_grad = False
 
-        # 定义指标
         self.k_list = [i for i in [1, 2, 3, 4, 5, 10]]
-        self.acc_metrics = torch.nn.ModuleList()
-        for k in self.k_list:
-            self.acc_metrics.append(Accuracy(top_k=k))
 
     def on_train_start(self):
         self.logger_begin()
@@ -52,8 +47,8 @@ class DistillModel(pl.LightningModule):
             dummy_input = torch.rand(size=(1, 3, 224, 224), device=self.device)
         else:
             dummy_input = torch.rand(size=(1, 77), device=self.device)
-        self.speed_test(self.student, dummy_input, pre_fix='stu_')
-        self.speed_test(self.teacher, dummy_input, pre_fix='tea_')
+        self.speed_test(self.student, dummy_input, prefix='stu_')
+        self.speed_test(self.teacher, dummy_input, prefix='tea_')
 
     @rank_zero_only
     def logger_begin(self):
@@ -66,7 +61,6 @@ class DistillModel(pl.LightningModule):
         elif isinstance(self.logger, TensorBoardLogger):
             self.logger.log_hyperparams(self.hparams, {"hp/stu_acc_top1": 0, "hp/stu_acc_top10": 0})
 
-    @rank_zero_only
     def speed_test(self, model, dummy_input, prefix):
         with torch.no_grad():
             flops, param = cal_flop(model, dummy_input)
@@ -78,13 +72,13 @@ class DistillModel(pl.LightningModule):
             f'{prefix}_std_times': std_syn,
             f'{prefix}_mean_fps': mean_fps
         }
-        self.log_dict(metric_dict, rank_zero_only=True, sync_dist=False)
+        self.log_dict(metric_dict, sync_dist=True)
 
     def forward(self, inputs):
 
         student_outs = self.student(inputs, self.need_return_para)
         teacher_outs = self.teacher(inputs, self.need_return_para)
-        if self.norm:
+        if self.hparams.norm:
             student_outs.last_representation /= student_outs.last_representation.norm(dim=-1, keepdim=True)
             teacher_outs.last_representation /= teacher_outs.last_representation.norm(dim=-1, keepdim=True)
         return student_outs, teacher_outs
@@ -97,7 +91,7 @@ class DistillModel(pl.LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        imgs, texts, sentence = batch
+        imgs, texts, _ = batch
         if self.hparams.model_type == 'text':
             inputs, contrary_rep = texts, imgs
         else:
