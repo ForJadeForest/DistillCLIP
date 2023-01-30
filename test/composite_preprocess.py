9df11ea0215ca1f80abb7f929a5f8fbec80b427d
SjokerLily <2857490843@qq.com>
2023-01-30 19:31:53 +0800

    todo:download rating file; image path

--- test/composite_preprocess.py
+++ test/composite_preprocess.py
@@ -1,12 +1,13 @@
 '''preprocesses the composite corpus.
 
 '''
-# from google_drive_downloader import GoogleDriveDownloader as gdd
+from google_drive_downloader import GoogleDriveDownloader as gdd
 import os
-# import tqdm
+import tqdm
 import collections
 import json
 import numpy as np
+import pandas as pd
 import csv
 
 def process_composite():
@@ -26,17 +27,50 @@ def process_composite():
                                             dest_path='./flickr8k/Flickr8k_text.zip',
                                             unzip=True,
                                             showsize=True)
-'''
+    '''
 
     # composite #
     ann_list = ['8k_correctness.csv', '8k_throughness.csv', '30k_correctness.csv', '30k_throughness.csv',
                'coco_correctness.csv', 'coco_throughness.csv']
-    composite_image2ann = collections.defaultdict(list)
+
+    # flickr8k #
+    flickr8k_image2ann = collections.defaultdict(list)
     captionid2caption = {}
+
+    with open('Flickr8k.token.txt') as f:
+        for line in f:
+            image, ann = line.strip().split('\t')
+            flickr8k_image2ann[image.split('#')[0]].append(ann)
+            captionid2caption[image] = ann
+
+    flickr8k_image2ann = {k.split('.')[0]: v for k, v in flickr8k_image2ann.items()}
+
+    # flickr30k #
+    flickr30k_image2ann = collections.defaultdict(list)
+
+    annotations = pd.read_table('results_20130124.token', sep='\t', header=None,
+                                names=['image', 'caption'])
+
+    for idx,line in enumerate(annotations['image']):
+        flickr30k_image2ann[line.split('#')[0]].append(annotations['caption'][idx])
+    flickr30k_image2ann = {k.split('.')[0]: v for k, v in flickr30k_image2ann.items()}
+
+
+    # coco #
+    mscoco_image2ann = collections.defaultdict(list)
+
+    with open("captions_val2014.json") as f:
+        data = {}
+        data.update(json.load(f))
+        for line in data['annotations']:
+            mscoco_image2ann['COCO_val2014_' + line['image_id'].zfill(12) + '.jpg'].append(line['caption'])
+
+    mscoco_image2ann = {k.split('.')[0]: v for k, v in mscoco_image2ann.items()}
 
     with open('composite.json', 'w') as f:
         for corpus in ann_list:
             all_index = {}
+            skip = 0
             with open(corpus, 'rb') as csvfile:
                 all_corpus = []
                 reader = csv.reader(csvfile)
@@ -44,8 +78,8 @@ def process_composite():
                 for item in raw_ann:
                     real_ann = item.split("http")[1].split(";")
                     all_corpus.append({
-                        'image_id': real_ann[0].split("/")[-1],
-                        'image_path': 'http'+ real_ann[0],
+                        'image_id': real_ann[0].split("/")[-1].split(".")[0],
+                        'image_path': 'http'+ real_ann[0],   # here need to be updated in real image dataset
                         'candidate1' : real_ann[1],
                         'candidate2' : real_ann[2],
                         'candidate3' : real_ann[3],
@@ -53,13 +87,45 @@ def process_composite():
                         'rating2' : real_ann[5],
                         'rating3' : real_ann[6]
                     })
-                for d in all_corpus:
-                    if d['image_id'] not in all_index:
-                        all_index[d['image_id']] = {
-                            'human_judgement': [],
-                            'image_id': d['image_id'],
-                            'image_path': d['image_path']}
-
+            for d in all_corpus:
+                if d['image_id'] not in all_index:
+                    if corpus[:2] == 'co':
+                        ground_truth = mscoco_image2ann[d['image_id']]
+                    elif corpus[:2] == '8k':
+                        ground_truth = flickr8k_image2ann[d['image_id']]
+                    else:
+                        ground_truth = flickr30k_image2ann[d['image_id']]
+                    all_index[d['image_id']] = {
+                        'human_judgement': [],
+                        'image_id': d['image_id'],
+                        'image_path': d['image_path'],
+                        'ground_truth': ground_truth}
+                if corpus[:2] == '8k':
+                    if d['candidate1'] in flickr8k_image2ann[d['image_id'].split('.')[0]]:
+                        skip += 1
+                    else:
+                        all_index[d['image_id']]['human_judgement'].append(
+                            {'image_id': d['image_id'],
+                             'image_path': d['image_path'],
+                             'caption': d['candidate1'],
+                             'rating': d['rating1']})
+                    if d['candidate2'] in flickr8k_image2ann[d['image_id'].split('.')[0]]:
+                        skip += 1
+                    else:
+                        all_index[d['image_id']]['human_judgement'].append(
+                            {'image_id': d['image_id'],
+                             'image_path': d['image_filepath'],
+                             'caption': d['candidate2'],
+                             'rating': d['rating2']})
+                    if d['candidate3'] in flickr8k_image2ann[d['image_id'].split('.')[0]]:
+                        skip += 1
+                    else:
+                        all_index[d['image_id']]['human_judgement'].append(
+                            {'image_id': d['image_id'],
+                             'image_path': d['image_filepath'],
+                             'caption': d['candidate3'],
+                             'rating': d['rating3']})
+                else:
                     all_index[d['image_id']]['human_judgement'].append(
                         {'image_id': d['image_id'],
                          'image_path': d['image_path'],
@@ -76,6 +142,7 @@ def process_composite():
                          'caption': d['candidate3'],
                          'rating': d['rating3']})
 
+            print('For 8k,we skip {} ground truth.(it should be 158)'.format(skip))
             print('For expert, we are dumping {} judgments between {} images'.format(
             len(all_corpus)*3,
             len(all_index)))
