14e11bf4889747a1ae25672e8c34595fc0126389
ForJadeForest <1113480910@qq.com>
2023-02-05 20:13:20 +0800

    add the pearsonr correction

--- test/coco_ex.py
+++ test/coco_ex.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 import pandas as pd
-import scipy
+from scipy import stats
 import torch.cuda
 from utils import get_model
 from clip_score import get_clip_score
@@ -59,14 +59,10 @@ def cal_metric(model_name, clip_model, images_filename, device, image_features):
 
 
 def main():
-    # clip_model = get_model()
-    clip_model, _ = load('ViT-B/32', 'cuda', r'/data/pyz/.cache')
-    clip_model.half()
-    clip_model.cuda()
-    clip_model.eval()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    clip_model = get_model(device)
     images_root_dir = Path(r'/data/pyz/data/mscoco/val2014')
-    image_paths, images_features = init_image_features(clip_model, images_root_dir, device)
+    images_filename, images_features = init_image_features(clip_model, images_root_dir, device)
 
     human_metric = pd.read_csv('./test_dataset/coco_captioning_challenge/leaderboard.csv').dropna(axis=1)
     human_metric = human_metric.set_index('Unnamed: 1').T
@@ -74,16 +70,19 @@ def main():
     human_metric_res_m1 = []
     human_metric_res_m2 = []
     for model_name in model_list:
-        mean_score, per_score, _ = cal_metric(model_name, clip_model, image_paths, device, images_features)
+        mean_score, per_score, _ = cal_metric(model_name, clip_model, images_filename, device, images_features)
         clip_score_res.append(mean_score)
         human_metric_res_m1.append(human_metric[model2company[model_name]]['M1'])
         human_metric_res_m2.append(human_metric[model2company[model_name]]['M2'])
 
-    m1_spearmanr, m1_p_value = scipy.stats.spearmanr(clip_score_res, human_metric_res_m1)
+    m1_spearmanr, m1_p_value = stats.spearmanr(clip_score_res, human_metric_res_m1)
     print(f'CLIPScore for M1 Spearmanr: {m1_spearmanr}, p-value: {m1_p_value}')
-    m2_spearmanr, m2_p_value = scipy.stats.spearmanr(clip_score_res, human_metric_res_m2)
+    m2_spearmanr, m2_p_value = stats.spearmanr(clip_score_res, human_metric_res_m2)
     print(f'CLIPScore for M2 Spearmanr: {m2_spearmanr}, p-value: {m2_p_value}')
-
+    m1_pearsonr, m1_p_value = stats.pearsonr(clip_score_res, human_metric_res_m1)
+    print(f'CLIPScore for M1 pearsonr: {m1_pearsonr}, p-value: {m1_p_value}')
+    m2_pearsonr, m2_p_value = stats.pearsonr(clip_score_res, human_metric_res_m2)
+    print(f'CLIPScore for M2 pearsonr: {m2_pearsonr}, p-value: {m2_p_value}')
 
 if __name__ == '__main__':
     main()
