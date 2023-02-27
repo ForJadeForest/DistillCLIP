9822fa27ddfb01f4ae5a76a41c1cb059cb813b12
JadeForest <1113480910@qq.com>
2023-02-27 14:43:13 +0800

    get the data in the beginning to avoid the random result because the sample of refs

--- test/pascal-50s_ex.py
+++ test/pascal-50s_ex.py
@@ -3,7 +3,7 @@ from random import sample
 import numpy as np
 import torch
 from test.preprocess.pascal50s_ex_dataset import Pascal50sDataset
-from test.clip_score import get_clip_score, get_ref_clip_score
+from test.clip_score import get_clip_score, get_ref_clip_score, get_all_clip_score
 from test.utils import get_model, get_args, total_ex
 
 idx2cat = {1: 'HC', 2: 'HI', 3: 'HM', 4: 'MM'}
@@ -24,33 +24,74 @@ def cal_acc(score1, score2, label_list, category_list):
     return acc
 
 
-def logout(acc_list, repeat_times):
+def logout(acc_list):
     print('=' * 80)
     res = {k: 0 for k in idx2cat.values()}
     for key in idx2cat.values():
         for acc in acc_list:
             res[key] += acc[key]
-        res[key] /= repeat_times
         res[key] = round(res[key] * 100, 2)
 
     print('the final result: ')
 
     for k, v in res.items():
         print(f'the result {k} acc: {v}')
-
-    print(f'the mean value of the acc is {sum(res.values()) / len(res.values())}')
+    mean_score = sum(res.values()) / len(res.values())
+    res['mean'] = mean_score
+    print(f'the mean value of the acc is {mean_score}')
     print('=' * 80)
     return res
 
 
-def cal_clip_score(pascal_dataset, model, device):
+def cal_clip_score(model, device,
+                   image_path_list, candidate1_list, candidate2_list,
+                   refs_list, label_list, category_list):
+    with torch.autocast('cuda'):
+        _, score1, _, ref_score1, = get_all_clip_score(model, image_path_list, refs_list, candidate1_list, device)
+        _, score2, _, ref_score2, = get_all_clip_score(model, image_path_list, refs_list, candidate2_list, device)
+
+    acc = cal_acc(score1, score2, label_list, category_list)
+    ref_acc = cal_acc(ref_score1, ref_score2, label_list, category_list)
+    return acc, ref_acc
+
+
+def cal_one_model_res(clip_model, device, *data_args):
+    acc_list = []
+    ref_acc_list = []
+
+    acc, ref_acc = cal_clip_score(clip_model, device, *data_args)
+    acc_list.append(acc)
+    ref_acc_list.append(ref_acc)
+
+    final_res = {}
+    print('The no ref acc result')
+    final_res['Pascal-50s CLIP-S'] = logout(acc_list)
+    print('The ref acc result')
+    final_res['Pascal-50s Ref-CLIP-S'] = logout(ref_acc_list)
+    return final_res
+
+
+def main(args, *data_args):
+    device = args.device
+    image_path = args.image_path
+    text_path = args.text_path
+    clip_path = args.clip_path
+    load_teacher = args.load_teacher
+    model = get_model(device, load_teacher, clip_path, image_path, text_path)
+
+    return cal_one_model_res(model, device, *data_args)
+
+
+if __name__ == '__main__':
+    args = get_args()
+    dataset = Pascal50sDataset(root='test_dataset/Pascal-50s', voc_path='/data/pyz/data/VOC/VOC2010')
     image_path_list = []
     candidate1_list = []
     candidate2_list = []
     refs_list = []
     category_list = []
     label_list = []
-    for data in pascal_dataset:
+    for data in dataset:
         image, candidate1, candidate2, refs, category, label = data
         image_path_list.append(image)
         candidate1_list.append(candidate1)
@@ -58,45 +99,6 @@ def cal_clip_score(pascal_dataset, model, device):
         refs_list.append(sample(refs, 5))
         category_list.append(category)
         label_list.append(label)
-    with torch.autocast('cuda'):
-        score1 = get_clip_score(model, image_path_list, candidate1_list, device)[1]
-        score2 = get_clip_score(model, image_path_list, candidate2_list, device)[1]
-        ref_score1 = np.array(get_ref_clip_score(model, image_path_list, refs_list, candidate1_list, device)[1])
-        ref_score2 = np.array(get_ref_clip_score(model, image_path_list, refs_list, candidate2_list, device)[1])
 
-    acc = cal_acc(score1, score2, label_list, category_list)
-    ref_acc = cal_acc(ref_score1, ref_score2, label_list, category_list)
-    return acc, ref_acc
-
-
-def cal_one_model_res(clip_model, device, repeat_times=1):
-    acc_list = []
-    ref_acc_list = []
-
-    for i in range(repeat_times):
-        dataset = Pascal50sDataset(root='test_dataset/Pascal-50s', voc_path='/data/pyz/data/VOC/VOC2010')
-        acc, ref_acc = cal_clip_score(dataset, clip_model, device)
-        acc_list.append(acc)
-        ref_acc_list.append(ref_acc)
-
-    final_res = {}
-    print('The no ref acc result')
-    final_res['Pascal-50s CLIP-S'] = logout(acc_list, repeat_times)
-    print('The ref acc result')
-    final_res['Pascal-50s Ref-CLIP-S'] = logout(ref_acc_list, repeat_times)
-    return final_res
-
-
-def main(args, repeat_times):
-    device = args.device
-    image_path = args.image_path
-    text_path = args.text_path
-    clip_path = args.clip_path
-    load_teacher = args.load_teacher
-    model = get_model(device, load_teacher, clip_path, image_path, text_path)
-    return cal_one_model_res(model, device, repeat_times=repeat_times)
-
-
-if __name__ == '__main__':
-    args = get_args()
-    total_ex(args, main, repeat_times=1)
+    total_ex(args, main, image_path_list, candidate1_list, candidate2_list,
+             refs_list, label_list, category_list)
