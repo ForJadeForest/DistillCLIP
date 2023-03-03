f2cfdb4791d11a5613da5614ea6ecf1ba28ea145
JadeForest <1113480910@qq.com>
2023-03-03 18:15:43 +0800

    add other n-gram-based metrics

--- test/coco_ex.py
+++ test/coco_ex.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 from scipy import stats
 import torch.cuda
-from utils import get_model, get_args, total_ex
+from utils import get_model, get_args, total_ex, get_all_metrics
 from clip_score import extract_all_images, get_all_clip_score
 
 model2company = {
@@ -64,8 +64,8 @@ def cal_metric(model_name, clip_model, images_filename, device, image_features):
 
     with torch.autocast('cuda' if 'cuda' in device else 'cpu'):
         res, _, ref_res, _ = get_all_clip_score(clip_model, image_features, ref_text, text, device)
-
-    return res, ref_res
+    other_metrics = get_all_metrics(ref_text, text)
+    return res, ref_res, other_metrics
 
 
 def cal_coco_ex(model, device):
@@ -79,15 +79,60 @@ def cal_coco_ex(model, device):
     ref_clip_score_res = []
     human_metric_res_m1 = []
     human_metric_res_m2 = []
+    bleu_score = []
+    meteor_score = []
+    cider_score = []
+    spice_score = []
+    rouge_score = []
     for model_name in model_list:
-        mean_score, ref_mean_score = cal_metric(model_name, model, images_filename, device, images_features)
+        mean_score, ref_mean_score, other_metric = cal_metric(model_name, model, images_filename, device, images_features)
         clip_score_res.append(mean_score)
         ref_clip_score_res.append(ref_mean_score)
         human_metric_res_m1.append(human_metric[model2company[model_name]]['M1'])
         human_metric_res_m2.append(human_metric[model2company[model_name]]['M2'])
+        bleu_score.append(other_metric['bleu'][-1])
+        meteor_score.append(other_metric['meteor'])
+        cider_score.append(other_metric['cider'])
+        spice_score.append(other_metric['spice'])
+        rouge_score.append(other_metric['rouge'])
 
+    bleu_score_res = {}
+    meteor_score_res = {}
+    cider_score_res = {}
+    spice_score_res = {}
+    rouge_score_res = {}
     clip_s_res = {}
     ref_clip_s_res = {}
+
+    m1_spearmanr, m1_p_value = stats.spearmanr(bleu_score, human_metric_res_m1)
+    bleu_score_res['M1 Spearman'] = round(100 * m1_spearmanr, 2)
+    m2_spearmanr, m1_p_value = stats.spearmanr(bleu_score, human_metric_res_m2)
+    bleu_score_res['M2 Spearman'] = round(100 * m2_spearmanr, 2)
+
+
+    m1_spearmanr, m1_p_value = stats.spearmanr(meteor_score, human_metric_res_m1)
+    meteor_score_res['M1 Spearman'] = round(100 * m1_spearmanr, 2)
+    m2_spearmanr, m1_p_value = stats.spearmanr(meteor_score, human_metric_res_m2)
+    meteor_score_res['M2 Spearman'] = round(100 * m2_spearmanr, 2)
+
+
+    m1_spearmanr, m1_p_value = stats.spearmanr(cider_score, human_metric_res_m1)
+    cider_score_res['M1 Spearman'] = round(100 * m1_spearmanr, 2)
+    m2_spearmanr, m1_p_value = stats.spearmanr(cider_score, human_metric_res_m2)
+    cider_score_res['M2 Spearman'] = round(100 * m2_spearmanr, 2)
+
+
+    m1_spearmanr, m1_p_value = stats.spearmanr(spice_score, human_metric_res_m1)
+    spice_score_res['M1 Spearman'] = round(100 * m1_spearmanr, 2)
+    m2_spearmanr, m1_p_value = stats.spearmanr(spice_score, human_metric_res_m2)
+    spice_score_res['M2 Spearman'] = round(100 * m2_spearmanr, 2)
+
+    m1_spearmanr, m1_p_value = stats.spearmanr(rouge_score, human_metric_res_m1)
+    rouge_score_res['M1 Spearman'] = round(100 * m1_spearmanr, 2)
+    m2_spearmanr, m1_p_value = stats.spearmanr(rouge_score, human_metric_res_m2)
+    rouge_score_res['M2 Spearman'] = round(100 * m2_spearmanr, 2)
+
+
 
     m1_spearmanr, m1_p_value = stats.spearmanr(clip_score_res, human_metric_res_m1)
     print(f'CLIPScore for M1 Spearmanr: {m1_spearmanr}, p-value: {m1_p_value}')
@@ -121,8 +166,15 @@ def cal_coco_ex(model, device):
     print(f'Ref CLIPScore for M2 pearsonr: {ref_m2_pearsonr}, p-value: {m2_p_value}')
     ref_clip_s_res['M2 Pearson'] = round(100 * ref_m2_pearsonr, 2)
 
-    final_res['CLIP-S-COCO'] = clip_s_res
-    final_res['Ref-CLIP-S-COCO'] = ref_clip_s_res
+    final_res['CLIP-S'] = clip_s_res
+    final_res['Ref-CLIP-S'] = ref_clip_s_res
+
+    final_res['bleu'] = bleu_score_res
+    final_res['meteor'] = meteor_score_res
+    final_res['cider'] = cider_score_res
+    final_res['spice'] = spice_score_res
+    final_res['rouge'] = rouge_score_res
+
     return final_res
 
 
