3948ac62bb46b02a168b3c84531b2f23453f4600
JadeForest <1113480910@qq.com>
2023-03-03 18:16:53 +0800

    delete coco ex

--- test/run_all_test.py
+++ test/run_all_test.py
@@ -1,6 +1,6 @@
 import os
 
-python_script_list = ['flickr8k_ex.py', 'composite.py', 'pascal-50s_ex.py', 'coco_ex.py', 'FOIL_allucination.py']
+python_script_list = ['flickr8k_ex.py', 'composite.py', 'pascal-50s_ex.py', 'FOIL_allucination.py']
 
 for script in python_script_list:
     os.system(f'~/.conda/envs/pyz/bin/python {script}')
