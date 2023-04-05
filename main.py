e7cc19e4f659fb226d46f54138d9d4a2ab895195
YingzhePeng <yingzhepeng@foxmail.com>
2023-04-05 13:31:36 +0800

    add IterableDatasetProgressBar

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from pytorch_lightning.callbacks import TQDMProgressBar
+
 from model import *
 from data import *
 import pytorch_lightning as pl
 import torch
 from pytorch_lightning.cli import LightningCLI
 from pytorch_lightning.accelerators import find_usable_cuda_devices
-from transformers.optimization import get_cosine_schedule_with_warmup
 import os
+
+
 # os.environ['JSONARGPARSE_DEBUG'] = 'true'
 # os.environ['TORCH_DISTRIBUTED_DEBUG'] = 'INFO'
 
@@ -21,5 +24,16 @@ class MyLightningCLI(LightningCLI):
         }
 
 
+class IterableDatasetProgressBar(TQDMProgressBar):
+    def __init__(self, total_length: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.total = total_length
+
+    def init_train_tqdm(self):
+        bar = super().init_validation_tqdm()
+        bar.total = self.total
+        return bar
+
+
 cli = MyLightningCLI(seed_everything_default=2022, save_config_callback=None,
-                     trainer_defaults={'devices': find_usable_cuda_devices(4)})
+                     trainer_defaults={'devices': find_usable_cuda_devices(2)})
