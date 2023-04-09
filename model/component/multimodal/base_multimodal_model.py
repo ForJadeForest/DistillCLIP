0d95f41dd3a71224cd916cc834610ae647c5f546
YingzhePeng <yingzhepeng@foxmail.com>
2023-04-09 12:45:43 +0800

    support the clip model

--- model/component/multimodal/base_multimodal_model.py
+++ model/component/multimodal/base_multimodal_model.py
@@ -2,34 +2,38 @@ from torch import nn
 from model.component.output import MultiModalOutput
 from model.component.output import TextTransformerOutput, VisionTransformerOutput
 from lavis.models import load_model
-
+from lavis import BlipFeatureExtractor, CLIP
 
 class MultiModalBaseModel(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, device, **kwargs):
         super().__init__()
-        self.model = load_model(**kwargs)
+        self.model = load_model(device=device, **kwargs)
 
-    def extract_image_features(self, image):
+    def encode_image(self, image):
         samples = {'image': image}
-        return self.model.extract_features(samples, mode='image').image_features
+        try:
+            image_feature = self.model.extract_features(samples, mode='image').image_embeds_proj[:, 0, :]
+        except:
+            image_feature = self.model.extract_features(samples)
+        return VisionTransformerOutput(last_representation=image_feature)
 
-    def extract_text_features(self, text):
+    def encode_text(self, text):
         samples = {'text_input': text}
-        return self.model.extract_text_features(samples).text_embeds_proj
+        try:
+            text_feature = self.model.extract_features(samples, mode='text').text_embeds_proj[:, 0, :]
+        except:
+            text_feature = self.model.extract_features(samples)
+        return TextTransformerOutput(last_representation=text_feature)
 
     def forward(self, text, image):
-        text_features = self.extract_text_features(text)
-        image_features = self.extract_image_features(image)
+        text_features = self.encode_text(text)
+        image_features = self.encode_image(image)
+        i2t_logits = image_features.last_representation @ text_features.last_representation.t()
+        t2i_logits = text_features.last_representation @ image_features.last_representation.t()
 
-        text_output = TextTransformerOutput(
-            last_representation=text_features[0],
-            last_layer_output=text_features
-        )
-        visual_output = VisionTransformerOutput(
-            last_representation=image_features[0],
-            last_layer_output=image_features
-        )
         return MultiModalOutput(
-            text_output=text_output,
-            visual_output=visual_output
+            text_output=text_features,
+            visual_output=image_features,
+            i2t_logits=i2t_logits,
+            t2i_logits=t2i_logits
         )
