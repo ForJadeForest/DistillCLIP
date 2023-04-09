8812c1380652908e39cbb5e43272caf129e77a9f
YingzhePeng <yingzhepeng@foxmail.com>
2023-04-09 21:33:55 +0800

    change the reduction mode

--- model/loss_component/soft_label.py
+++ model/loss_component/soft_label.py
@@ -5,12 +5,12 @@ from torch.nn import functional as f
 class SoftLabel(nn.Module):
     def __init__(self, temperature):
         super(SoftLabel, self).__init__()
-        self.loss = nn.KLDivLoss(reduction='sum')
+        self.loss = nn.KLDivLoss(reduction='batchmean')
         self.temperature = temperature
 
     def forward(self, stu_logits, tea_logits):
         logits_kl_loss = self.loss(
-            f.softmax(stu_logits / self.temperature, dim=1).log(),
+            f.log_softmax(stu_logits / self.temperature, dim=1),
             f.softmax(tea_logits / self.temperature, dim=1)
         ) * self.temperature ** 2
         return logits_kl_loss
