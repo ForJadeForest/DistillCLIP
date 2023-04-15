a034aadb4ba409ddf22bc5c0aa5eeffd18c09559
YingzhePeng <yingzhepeng@foxmail.com>
2023-04-15 23:16:24 +0800

    add label cache to reduce the time

--- model/loss_component/hard_label.py
+++ model/loss_component/hard_label.py
@@ -6,7 +6,10 @@ class HardLabel(nn.Module):
     def __init__(self):
         super(HardLabel, self).__init__()
         self.loss = nn.CrossEntropyLoss(reduction='mean')
+        self.label_cache = None
 
     def forward(self, stu_logits):
-        label = torch.arange(stu_logits.shape[0], device=stu_logits.device)
-        return self.loss(stu_logits, label)
+        if self.label_cache is None:
+            self.label_cache = torch.arange(stu_logits.shape[0], device=stu_logits.device)
+
+        return self.loss(stu_logits, self.label_cache)
