d743e4207092c4082f2afa592b97d4a95e8030c4
YingzhePeng <yingzhepeng@foxmail.com>
2023-04-17 14:53:02 +0800

    add dim_map for the different dim between student and teacher

--- model/loss_component/out_cos.py
+++ model/loss_component/out_cos.py
@@ -1,11 +1,17 @@
 import torch
+from typing import List, Optional
 from torch import nn
 
 
 class OutCosLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, dim_map: Optional[List] = None):
         super().__init__()
         self.loss = nn.CosineEmbeddingLoss()
+        self.aline_linear = None
+        if dim_map:
+            self.aline_linear = nn.Linear(dim_map[0], dim_map[1])
 
-    def forward(self, stu_out: torch.Tensor, tea_out):
+    def forward(self, stu_out: torch.Tensor, tea_out: torch.Tensor):
+        if self.aline_linear:
+            stu_out = self.aline_linear(stu_out)
         return self.loss(stu_out, tea_out, torch.ones(len(stu_out), device=stu_out.device))
--- model/loss_component/out_l1.py
+++ model/loss_component/out_l1.py
@@ -1,10 +1,15 @@
 from torch import nn
-
+from typing import  Optional, List
 
 class OutL1Loss(nn.Module):
-    def __init__(self):
+    def __init__(self, dim_map: Optional[List]):
         super().__init__()
         self.loss = nn.L1Loss()
+        self.aline_linear = None
+        if dim_map:
+            self.aline_linear = nn.Linear(dim_map[0], dim_map[1])
 
     def forward(self, stu_out, tea_out):
+        if self.aline_linear:
+            stu_out = self.aline_linear(stu_out)
         return self.loss(stu_out, tea_out)
