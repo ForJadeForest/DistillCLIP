170374e643a615404c8f3d24a56a4c04a0720770
YingzhePeng <yingzhepeng@foxmail.com>
2023-04-24 22:38:08 +0800

    delete label_cache

--- model/loss_component/hard_label.py
+++ model/loss_component/hard_label.py
@@ -6,10 +6,7 @@ class HardLabel(nn.Module):
     def __init__(self):
         super(HardLabel, self).__init__()
         self.loss = nn.CrossEntropyLoss(reduction='mean')
-        self.label_cache = None
 
     def forward(self, stu_logits):
-        if self.label_cache is None:
-            self.label_cache = torch.arange(stu_logits.shape[0], device=stu_logits.device)
-
-        return self.loss(stu_logits, self.label_cache)
+        labels = torch.arange(stu_logits.shape[0], device=stu_logits.device)
+        return self.loss(stu_logits, labels)
