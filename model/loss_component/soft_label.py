667e502935c9b79ddb1e50d68c2c781789fbbb94
YingzhePeng <yingzhepeng@foxmail.com>
2023-04-24 22:39:16 +0800

    delete temperature

--- model/loss_component/soft_label.py
+++ model/loss_component/soft_label.py
@@ -10,4 +10,4 @@ class SoftLabel(nn.Module):
     def forward(self, stu_logits, tea_logits):
         def dist_loss(teacher_logits, student_logits):
             return -(teacher_logits.softmax(dim=1) * student_logits.log_softmax(dim=1)).sum(dim=1).mean(dim=0)
-        return dist_loss(tea_logits / self.temperature, stu_logits / self.temperature) * self.temperature ** 2
+        return dist_loss(tea_logits / self.temperature, stu_logits / self.temperature)
